54c2214dbad938e91401cae28bae460bb125bb0b
Leandro Ciric <lecirics@gmail.com>
2023-05-19 23:40:28 -0300

    staging

--- contracts/DocumentAuthentication.sol
+++ contracts/DocumentAuthentication.sol
@@ -4,45 +4,15 @@ pragma solidity ^0.8.9;
 //  import "hardhat/console.sol";
 
 contract DocumentAuthentication {
-    mapping(bytes32 => string) private hashToOwner;
-    mapping(string => bytes32) private ownerToHash;
-    mapping(string => bytes32[]) private ownerToHashList;
+    mapping(bytes32 => bool) private hashes;
 
-    function storeHash(bytes32 hash, string memory owner) public {
-        require(bytes(hashToOwner[hash]).length == 0, "Hash already stored");
-
-        bytes32 oldHash = ownerToHash[owner];
-        if (oldHash != 0) {
-            ownerToHashList[owner].push(hash);
-        } else {
-            bytes32[] memory hashList = new bytes32[](1);
-            hashList[0] = hash;
-            ownerToHashList[owner] = hashList;
-        }
-        hashToOwner[hash] = owner;
-        ownerToHash[owner] = hash;
+    function storeHash(bytes32 hash) public {
+        require(!hashes[hash], "Hash already stored");
+        hashes[hash] = true;
     }
 
-    function verifyHash(bytes32 hash) public view returns (string memory) {
-        return hashToOwner[hash];
-    }
-
-    function getHashes(string memory owner) public view returns (bytes32[] memory) {
-        return ownerToHashList[owner];
-    }
-
-    function changeOwner(bytes32 hash, string memory newOwner) public {
-        require(bytes(hashToOwner[hash]).length > 0, "Hash not found");
-        bytes32 oldHash = ownerToHash[newOwner];
-        if (oldHash != 0) {
-            ownerToHashList[newOwner].push(hash);
-        } else {
-            bytes32[] memory hashList = new bytes32[](1);
-            hashList[0] = hash;
-            ownerToHashList[newOwner] = hashList;
-        }
-        hashToOwner[hash] = newOwner;
-        ownerToHash[newOwner] = hash;
+    function verifyHash(bytes32 hash) public view returns (bool) {
+        return hashes[hash];
     }
 }
 
