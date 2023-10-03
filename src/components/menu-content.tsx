549046b9fde84ca8599e8a8ffc89ee62ba25d20d
Leandro Ciric <lecirics@gmail.com>
2023-10-03 20:15:28 -0300

    feat: added authenticate document page

--- src/components/menu-content.tsx
+++ src/components/menu-content.tsx
@@ -3,31 +3,36 @@ import {
   HomeIcon,
   SparklesIcon,
 } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 export function MenuContent() {
   return (
     <div>
       <div className="flex flex-row items-center gap-x-3">
-        <SparklesIcon className="h-12 w-12 rounded-full text-primary"></SparklesIcon>
+        <SparklesIcon className="text-primary h-12 w-12 rounded-full"></SparklesIcon>
         <p className="text-xl font-bold capitalize">DocGuard</p>
       </div>
 
       <div className="mt-12 flex flex-col gap-6">
-        <nav
-          tabIndex={0}
-          className="flex w-full cursor-pointer flex-row gap-4 rounded-md p-4 hover:bg-neutral-100"
-        >
-          <HomeIcon className="h-6 w-6"></HomeIcon>
-          <p className="text-base ">Meus documentos</p>
-        </nav>
+        <Link href="/my-documents" passHref>
+          <nav
+            tabIndex={0}
+            className="flex w-full cursor-pointer flex-row gap-4 rounded-md p-4 hover:bg-neutral-100"
+          >
+            <HomeIcon className="h-6 w-6"></HomeIcon>
+            <p className="text-base ">Meus documentos</p>
+          </nav>
+        </Link>
 
-        <nav
+        <Link
+          href="/authenticate"
+          passHref
           tabIndex={0}
           className="flex w-full cursor-pointer flex-row gap-4 rounded-md p-4 hover:bg-neutral-100"
         >
           <DocumentTextIcon className="h-6 w-6"></DocumentTextIcon>
           <p className="text-base ">Autenticar documento</p>
-        </nav>
+        </Link>
 
         <nav
           tabIndex={0}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans serif'],
@@ -26,6 +35,14 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
         overlayShow: {
           from: { opacity: 0 },
           to: { opacity: 1 },
@@ -36,10 +53,12 @@ module.exports = {
         },
       },
       animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
         overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
         contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 }
