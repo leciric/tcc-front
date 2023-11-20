85a9508aba5cee4afb6062e4a3521fe85d2c7615
Leandro Ciric <lecirics@gmail.com>
2023-11-19 21:32:39 -0300

    fix(token): added log on token

--- src/app/my-documents/page.tsx
+++ src/app/my-documents/page.tsx
@@ -3,11 +3,18 @@
 import { Document } from '@/components/document'
 import { Layout } from '@/components/layout'
 import { useAuth } from '@clerk/nextjs'
+import { useEffect } from 'react'
 
 export default function MyDocuments() {
   const { getToken } = useAuth()
 
-  console.log(getToken())
+  useEffect(() => {
+    async function handleGetToken() {
+      console.log(await getToken())
+    }
+
+    handleGetToken()
+  }, [getToken])
   return (
     <Layout>
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-3 md:grid-cols-3 lg:gap-8 xl:grid-cols-4">
